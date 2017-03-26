c288805b956dbf1bae05d685da50cae3317922c9
Xin Hao Zhang <xinhao@v1020-wn-201-216.campus-dynamic.uwaterloo.ca>
2017-03-26 00:41:00 -0400

    Implement message generation and movement

--- js/game.js
+++ js/game.js
@@ -1,20 +1,23 @@
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1280, 720, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
     game.load.image('space', 'assets/deep-space.jpg');
     game.load.image('bullet', 'assets/bullets.png');
     game.load.image('ship', 'assets/ship.png');
+    game.load.image('msg', 'assets/msg.png');
 
 }
 
 var sprite;
 var cursors;
 
+var msgs;
 var bullet;
 var bullets;
 var bulletTime = 0;
+var genTime = 100;
 
 function create() {
 
@@ -34,9 +37,20 @@ function create() {
     bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
     //  All 40 of them
-    bullets.createMultiple(40, 'bullet');
     bullets.setAll('anchor.x', 0.5);
     bullets.setAll('anchor.y', 0.5);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+
+
+    //msgs
+    msgs = game.add.group();
+    msgs.enableBody = true;
+    msgs.physicsBodyType = Phaser.Physics.ARCADE;
+    msgs.createMultiple(30, 'msg');
+    msgs.setAll('anchor.x', 0.5);
+    msgs.setAll('anchor.y', 0.5);
+
 
     //  Our player ship
     sprite = game.add.sprite(300, 300, 'ship');
@@ -83,9 +97,41 @@ function update() {
     {
         sprite.body.angularVelocity = 0;
     }
-
+    if (genTime == 100) {
+        createMsg();
+    }
+    if (genTime == 0) {
+        genTime = 100;
+    } else {
+        --genTime;
+    }
 
 }
+
+ function createMsg () {
+    var side = game.rnd.integerInRange(-1, 5);
+    console.log(side);
+    var x, y;
+    if (side < 2) {
+        y = game.world.randomY;
+        x = -50;
+        if (side == 1) {
+            x = game.width + 50;
+        }
+    } else {
+        x = game.world.randomX;
+        y = -50;
+        if (side == 1) {
+            y = game.height + 50;
+        }
+    }
+    var msg = msgs.create(x, y, 'msg');
+    msg.body.collideWorldBounds = true;
+    msg.body.bounce.set(0.8);
+    game.physics.arcade.moveToXY(msg, game.world.centerX, game.world.centerY, 100, 0);
+    
+ }
+
 
 function fireBullet () {
 
@@ -96,7 +142,6 @@ function fireBullet () {
         if (bullet)
         {
             bullet.reset(sprite.body.x + 16, sprite.body.y + 16);
-            bullet.lifespan = 2000;
             bullet.rotation = sprite.rotation;
             game.physics.arcade.velocityFromRotation(sprite.rotation, 400, bullet.body.velocity);
             bulletTime = game.time.now + 50;
